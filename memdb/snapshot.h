c68f83e1db46fd015232444290f4657ec4c5a7ff
Santa Zhang <santa1987@gmail.com>
2014-03-06 20:31:04 -0500

    simplify

--- memdb/snapshot.h
+++ memdb/snapshot.h
@@ -89,13 +89,14 @@ public:
 
 };
 
+// note: this includes both writer and snapshots
 template <class Key, class Value, class Container, class Snapshot>
 struct snapshotset: public RefCounted {
     Container data;
     Snapshot* writer;
-    std::set<Snapshot*> reader;
+    std::set<Snapshot*> snapshots;
 
-    snapshotset(): writer(nullptr) {}
+    snapshotset(Snapshot* w): writer(w) {}
 
     // protected dtor as required by RefCounted
 protected:
@@ -130,144 +131,67 @@ public:
 
 private:
 
-    class Writer {
-        snapshot_sortedmap* ss_;
-    public:
-        Writer(snapshot_sortedmap* ss): ss_(ss) {}
-        void insert(const Key& key, const Value& value) {
-            // advance to next version
-            ss_->ver_++;
-            versioned_value<Value> vv(ss_->ver_, value);
-            insert_into_map(ss_->sss_->data, key, vv);
-        }
-
-        template <class Iterator>
-        void insert(Iterator begin, Iterator end) {
-            ss_->ver_++;
-            while (begin != end) {
-                versioned_value<Value> vv(ss_->ver_, begin->second);
-                insert_into_map(ss_->sss_->data, begin->first, vv);
-                ++begin;
-            }
-        }
-
-        void insert(kv_range range) {
-            ss_->ver_++;
-            while (range) {
-                std::pair<const Key*, const Value*> kv_pair = range.next();
-                versioned_value<Value> vv(ss_->ver_, *(kv_pair.second));
-                insert_into_map(ss_->sss_->data, *(kv_pair.first), vv);
-            }
-        }
-
-        void remove_key(const Key& key) {
-            ss_->ver_++;
-            if (ss_->has_snapshot()) {
-                for (auto it = ss_->sss_->data.lower_bound(key); it != ss_->sss_->data.upper_bound(key); ++it) {
-                    it->second.remove(ss_->ver_);
-                }
-            } else {
-                auto it = ss_->sss_->data.lower_bound(key);
-                while (it != ss_->sss_->data.upper_bound(key)) {
-                    it = ss_->sss_->data.erase(it);
-                }
-            }
-        }
-    };
-
-    snapshotset* sss_;
-
+    bool rdonly_;
     version_t ver_;
-
-    // doubly linked list of snapshots
-    mutable const snapshot_sortedmap* prev_;
-    mutable const snapshot_sortedmap* next_;
-
-    Writer* writer_;
+    snapshotset* sss_;
 
     void make_me_snapshot_of(const snapshot_sortedmap& src) {
         verify(ver_ < 0);
         ver_ = src.ver_;
-        verify(writer_ == nullptr);
-        writer_ = nullptr;
+        verify(rdonly_ == true);
         verify(sss_ == nullptr);
         sss_ = (snapshotset *) src.sss_->ref_copy();
-
-        // set the doubly linked list
-        if (src.prev_ == nullptr) {
-            verify(src.next_ == nullptr);
-            src.prev_ = this;
-            src.next_ = this;
-            this->prev_ = &src;
-            this->next_ = &src;
-        } else {
-            verify(src.next_ != nullptr);
-            // insert me after src
-            this->prev_ = &src;
-            this->next_ = src.next_;
-            src.next_->prev_ = this;
-            src.next_ = this;
-        }
+        sss_->snapshots.insert(this);
     }
 
     void destory_me() {
         collect_my_garbage();
 
-        // unlink me from the doubly linked list
-        if (this->prev_ != nullptr) {
-            verify(this->next_ != nullptr);
-            if (this->prev_ == this->next_) {
-                // special case for last element
-                this->prev_->next_ = nullptr;
-                this->next_->prev_ = nullptr;
-            } else {
-                this->prev_->next_ = this->next_;
-                this->next_->prev_ = this->prev_;
-            }
-            this->prev_ = nullptr;
-            this->next_ = nullptr;
+        if (sss_->writer == this) {
+            sss_->writer = nullptr;
+        } else {
+            sss_->snapshots.erase(this);
         }
 
-        if (writer_ != nullptr) {
-            delete writer_;
-            writer_ = nullptr;
-        }
         sss_->release();
         sss_ = nullptr;
+        rdonly_ = true;
         ver_ = -1;
     }
 
     // creating a snapshot
     snapshot_sortedmap(const snapshot_sortedmap& src, const snapshot::snapshot_marker&)
-            : sss_(nullptr), ver_(-1), prev_(nullptr), next_(nullptr), writer_(nullptr) {
+            : rdonly_(true), ver_(-1), sss_(nullptr) {
         make_me_snapshot_of(src);
     }
 
 public:
 
     // creating a new snapshot_sortedmap
-    snapshot_sortedmap(): ver_(0), prev_(nullptr), next_(nullptr) {
-        sss_ = new snapshotset;
-        writer_ = new Writer(this);
+    snapshot_sortedmap(): rdonly_(false), ver_(0) {
+        sss_ = new snapshotset(this);
     }
 
     snapshot_sortedmap(const snapshot_sortedmap& src)
-            : sss_(nullptr), ver_(-1), prev_(nullptr), next_(nullptr), writer_(nullptr) {
+            : rdonly_(false), ver_(-1), sss_(nullptr) {
         if (src.readonly()) {
             // src is a snapshot, make me a snapshot, too
+            rdonly_ = true;
+            ver_ = -1;
+            sss_ = nullptr;
             make_me_snapshot_of(src);
         } else {
-            sss_ = new snapshotset;
-            writer_ = new Writer(this);
-            writer_->insert(src.all());
+            rdonly_ = false;
+            ver_ = 0;
+            sss_ = new snapshotset(this);
+            insert(src.all());
         }
     }
 
     template <class Iterator>
-    snapshot_sortedmap(Iterator it_begin, Iterator it_end): ver_(0), prev_(nullptr), next_(nullptr) {
-        sss_ = new snapshotset;
-        writer_ = new Writer(this);
-        writer_->insert(it_begin, it_end);
+    snapshot_sortedmap(Iterator it_begin, Iterator it_end): rdonly_(false), ver_(0) {
+        sss_ = new snapshotset(this);
+        insert(it_begin, it_end);
     }
 
     ~snapshot_sortedmap() {
@@ -283,7 +207,10 @@ public:
     }
 
     bool readonly() const {
-        return writer_ == nullptr;
+        if (rdonly_) {
+            verify(sss_->writer != this);
+        }
+        return rdonly_;
     }
 
     const snapshot_sortedmap& operator= (const snapshot_sortedmap& src) {
@@ -292,90 +219,72 @@ public:
             if (src.readonly()) {
                 make_me_snapshot_of(src);
             } else {
-                verify(prev_ == nullptr);
-                verify(next_ == nullptr);
                 verify(ver_ == -1);
                 verify(sss_ == nullptr);
-                verify(writer_ == nullptr);
+                verify(rdonly_ == true);
+                rdonly_ = false;
                 ver_ = 0;
-                sss_ = new snapshotset;
-                writer_ = new Writer(this);
-                writer_->insert(src.all());
+                sss_ = new snapshotset(this);
+                insert(src.all());
             }
         }
         return *this;
     }
 
+    // snapshot: readonly
     bool has_snapshot() const {
-        if (prev_ == nullptr) {
-            verify(next_ == nullptr);
-            return false;
-        }
-        return true;
+        return !sss_->snapshots.empty();
     }
 
     snapshot_sortedmap snapshot() const {
         return snapshot_sortedmap(*this, snapshot::snapshot_marker());
     }
 
-    class SnapshotEnumerator: public Enumerator<const snapshot_sortedmap*> {
-        const snapshot_sortedmap* o_;
-        const snapshot_sortedmap* p_;
-        bool first_;
-        int count_;
-    public:
-        SnapshotEnumerator(const snapshot_sortedmap* ss): o_(ss), p_(ss), first_(true), count_(-1) {}
-        bool has_next() {
-            if (first_) {
-                return true;
-            }
-            return p_ != nullptr && p_ != o_;
-        }
-        const snapshot_sortedmap* next() {
-            const snapshot_sortedmap* ret = p_;
-            p_ = p_->next_;
-            first_ = false;
-            return ret;
-        }
-        int count() {
-            if (count_ > 0) {
-                return count_;
-            }
-            count_ = 0;
-            const snapshot_sortedmap* o = o_;
-            const snapshot_sortedmap* p = o_->next_;
-            if (p == nullptr) {
-                count_ = 1;
-            } else {
-                for (;;) {
-                    count_++;
-                    if (p == o) {
-                        break;
-                    }
-                    p = p->next_;
-                }
-            }
-            return count_;
-        }
-    };
-
-    SnapshotEnumerator all_snapshots() const {
-        return SnapshotEnumerator(this);
+    std::set<snapshot_sortedmap*> all_snapshots() const {
+        return this->sss_->snapshots;
     }
 
     void insert(const Key& key, const Value& value) {
         verify(!readonly());
-        writer_->insert(key, value);
+        ver_++;
+        versioned_value<Value> vv(ver_, value);
+        insert_into_map(sss_->data, key, vv);
     }
 
     template <class Iterator>
     void insert(Iterator begin, Iterator end) {
-        writer_->insert(begin, end);
+        verify(!readonly());
+        ver_++;
+        while (begin != end) {
+            versioned_value<Value> vv(ver_, begin->second);
+            insert_into_map(sss_->data, begin->first, vv);
+            ++begin;
+        }
+    }
+
+    void insert(kv_range range) {
+        verify(!readonly());
+        ver_++;
+        while (range) {
+            std::pair<const Key*, const Value*> kv_pair = range.next();
+            versioned_value<Value> vv(ver_, *(kv_pair.second));
+            insert_into_map(sss_->data, *(kv_pair.first), vv);
+        }
     }
 
     void remove_key(const Key& key) {
         verify(!readonly());
-        writer_->remove_key(key);
+        ver_++;
+        if (has_snapshot()) {
+            for (auto it = sss_->data.lower_bound(key); it != sss_->data.upper_bound(key); ++it) {
+                it->second.remove(ver_);
+            }
+        } else {
+            auto it = sss_->data.lower_bound(key);
+            while (it != sss_->data.upper_bound(key)) {
+                it = sss_->data.erase(it);
+            }
+        }
     }
 
     kv_range all() const {
@@ -406,10 +315,8 @@ private:
         }
 
         // ENHANCE faster garbage collection (on data only visible to this snapshot)
-        auto snapshots = all_snapshots();
-        while (snapshots) {
-            const snapshot_sortedmap* ss = snapshots.next();
-            if (ss != this && ss->ver_ <= this->ver_) {
+        for (auto& it : all_snapshots()) {
+            if (it != this && it->ver_ <= this->ver_) {
                 return;
             }
         }
--- test/test-snapshot.cc
+++ test/test-snapshot.cc
@@ -9,26 +9,28 @@ using namespace std;
 
 TEST(snapshot, snapshot_on_empty_table) {
     snapshot_sortedmap<int, string> data;
-    EXPECT_EQ(data.all_snapshots().count(), 1);
+    EXPECT_EQ(data.all_snapshots().size(), 0u);
     {
         snapshot_sortedmap<int, string> snapshot = data.snapshot();
-        EXPECT_EQ(data.all_snapshots().count(), 2);
-        EXPECT_EQ(snapshot.all_snapshots().count(), 2);
+        EXPECT_EQ(data.all_snapshots().size(), 1u);
+        EXPECT_EQ(snapshot.all_snapshots().size(), 1u);
         data.snapshot();
         data.snapshot();
         snapshot = snapshot.snapshot();
-        EXPECT_EQ(snapshot.all_snapshots().count(), 2);
+        EXPECT_EQ(snapshot.all_snapshots().size(), 1u);
         EXPECT_EQ(data.version(), 0);
         EXPECT_EQ(snapshot.version(), 0);
         EXPECT_TRUE(data.valid());
         EXPECT_TRUE(snapshot.valid());
         EXPECT_FALSE(data.readonly());
+
+        // make data a snapshot
         data = snapshot.snapshot();
         EXPECT_TRUE(data.readonly());
         EXPECT_TRUE(snapshot.readonly());
-        EXPECT_EQ(data.all_snapshots().count(), 2);
+        EXPECT_EQ(data.all_snapshots().size(), 2u);
     }
-    EXPECT_EQ(data.all_snapshots().count(), 1);
+    EXPECT_EQ(data.all_snapshots().size(), 1u);
 }
 
 static void print_range(snapshot_sortedmap<int, string>::kv_range range) {
