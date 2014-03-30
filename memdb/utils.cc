9142d41d2a2465f1fe13222c7b2e80f3821a3db0
Santa Zhang <santa1987@gmail.com>
2014-03-30 14:20:10 -0400

    minor cleanup on the int hash functions

--- memdb/utils.cc
+++ memdb/utils.cc
@@ -22,81 +22,41 @@ uint64_t stringhash64(const void* data, int len) {
     return hash_value[0] ^ hash_value[1];
 }
 
-// https://gist.github.com/badboy/6267743
-/*
-static inline uint32_t mix32_func1(uint32_t key) {
-    key = ~key + (key << 15); // key = (key << 15) - key - 1;
-    key = key ^ (key >> 12);
-    key = key + (key << 2);
-    key = key ^ (key >> 4);
-    key = key * 2057; // key = (key + (key << 3)) + (key << 11);
-    key = key ^ (key >> 16);
-    return key;
+// from MurmurHash3
+static inline uint32_t fmix32(uint32_t h) {
+    h ^= h >> 16;
+    h *= 0x85ebca6b;
+    h ^= h >> 13;
+    h *= 0xc2b2ae35;
+    h ^= h >> 16;
+    return h;
 }
 
-static inline uint32_t mix32_func2(uint32_t a) {
-    a = (a+0x7ed55d16) + (a<<12);
-    a = (a^0xc761c23c) ^ (a>>19);
-    a = (a+0x165667b1) + (a<<5);
-    a = (a+0xd3a2646c) ^ (a<<9);
-    a = (a+0xfd7046c5) + (a<<3);
-    a = (a^0xb55a4f09) ^ (a>>16);
-    return a;
-}
-*/
-
-static inline uint32_t mix32_func3(uint32_t key) {
-    uint32_t c2 = 0x27d4eb2d; // a prime or an odd constant
-    key = (key ^ 61) ^ (key >> 16);
-    key = key + (key << 3);
-    key = key ^ (key >> 4);
-    key = key * c2;
-    key = key ^ (key >> 15);
-    return key;
-}
-
-// http://stackoverflow.com/questions/664014/what-integer-hash-function-are-good-that-accepts-an-integer-hash-key
-/*
-static inline uint32_t mix32_func4(uint32_t k) {
-    k *= 357913941;
-    k ^= k << 24;
-    k += ~357913941;
-    k ^= k >> 31;
-    k ^= k << 31;
+// from MurmurHash3
+static inline uint64_t fmix64(uint64_t k) {
+    k ^= k >> 33;
+    k *= 0xff51afd7ed558ccd;
+    k ^= k >> 33;
+    k *= 0xc4ceb9fe1a85ec53;
+    k ^= k >> 33;
     return k;
-}
-
-static inline uint32_t mix32_func5(uint32_t x) {
-    x = ((x >> 16) ^ x) * 0x45d9f3b;
-    x = ((x >> 16) ^ x) * 0x45d9f3b;
-    x = ((x >> 16) ^ x);
-    return x;
-}
-*/
-
-static inline uint64_t mix64(uint64_t key) {
-    key = (~key) + (key << 21); // key = (key << 21) - key - 1;
-    key = key ^ (key >> 24);
-    key = (key + (key << 3)) + (key << 8); // key * 265
-    key = key ^ (key >> 14);
-    key = (key + (key << 2)) + (key << 4); // key * 21
-    key = key ^ (key >> 28);
-    key = key + (key << 31);
-    return key;
 }
 
 uint32_t inthash32(uint32_t* key, int key_length) {
     uint32_t h = 0;
+    // http://cstheory.stackexchange.com/questions/9639/how-did-knuth-derive-a
+    const uint32_t A = 0x9e3779b9;
     for (int i = 0; i < key_length; i++) {
-        h ^= mix32_func3(key[i]);
+        h = (h * A) ^ fmix32(key[i]);
     }
     return h;
 }
 
 uint64_t inthash64(uint64_t* key, int key_length) {
     uint64_t h = 0;
+    const uint64_t A = 0x9e3779b97f4a7c15;  // obtained from wolframalpha
     for (int i = 0; i < key_length; i++) {
-        h ^= mix64(key[i]);
+        h = (h * A) ^ fmix64(key[i]);
     }
     return h;
 }
--- test/test-utils.cc
+++ test/test-utils.cc
@@ -34,29 +34,3 @@ TEST(utils, inthash64) {
     EXPECT_NEQ(inthash64(1987, 1989), inthash64(1987, 1001));
     Log::debug("inthash64(1987, 1001) = %llu", inthash64(1987, 1001));
 }
-
-template<class T>
-const T& get_value(const T& v, std::true_type) {
-    return v;
-}
-
-template<class T>
-const typename T::second_type& get_value(const T& v, std::false_type) {
-    return v.second;
-}
-
-template <class Container>
-static void print_container_values(const Container& container) {
-    for (auto it = container.begin(); it != container.end(); ++it) {
-        cout << "value in container: " << get_value(*it, std::is_same<int, typename Container::value_type>()) << endl;
-    }
-}
-
-TEST(utils, value_enumerator) {
-    print_container_values(set<int>({1, 2, 3, 4}));
-    print_container_values(vector<int>({1, 2, 3, 4}));
-    map<int, int> m;
-    m[1] = 2;
-    m[3] = 4;
-    print_container_values(m);
-}
